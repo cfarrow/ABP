0f1709440677e37686a098551ecca608321f65cf
cfarrow <farrowch@gmail.com>
2025-04-28 12:47:14 -0500

    Test other word manipulation methods.

--- src/tests/test_wordops.cpp
+++ src/tests/test_wordops.cpp
@@ -9,17 +9,87 @@
 // no shift at all. (This has been fixed by explicityly creating a size_t{1} and
 // then shifting.) As a reasult, half of all sites stored in the upper-half of a
 // size_t word could not be modified by the code.  This test catches that error.
-TEST(wordopsTest, testbitmask)
+
+const size_t shift_3 = 0b1000;
+const size_t shift_19 = 0b10000000000000000000;
+const size_t shift_41 = 0b100000000000000000000000000000000000000000;
+
+
+TEST(wordopsTest, test_bit_mask)
 {
-    size_t shift, answer;
+    EXPECT_EQ(shift_3, bit_mask(3));
 
-    shift = 1;
-    answer = 0b10;
-    ASSERT_EQ(answer, bit_mask(shift));
-
-    if(sizeof(size_t) > 4) {
-        shift = 41;
-        answer = 0b100000000000000000000000000000000000000000;
-        ASSERT_EQ(answer, bit_mask(shift));
+    if(word_size > 32) {
+        EXPECT_EQ(shift_41, bit_mask(41));
     }
+}
+
+
+TEST(wordopsTest, test_get_site_value)
+{
+    size_t* words = new size_t[8]{0, 0, 0, 0};
+    size_t site_index;
+
+    site_index = 2 * word_size + 3;
+    EXPECT_EQ(false, get_site_value(site_index, words));
+
+    words[2] |= shift_3;
+    EXPECT_EQ(true, get_site_value(site_index, words));
+
+    if(word_size > 32) {
+        site_index = word_size + 41;
+        EXPECT_EQ(false, get_site_value(site_index, words));
+
+        words[1] |= shift_41;
+        EXPECT_EQ(true, get_site_value(site_index, words));
+    }
+}
+
+TEST(wordopsTest, test_set_site_value)
+{
+    size_t* words = new size_t[8]{0, 0, 0, 0};
+    size_t site_index;
+
+    // Test flipping a single site.
+    site_index = 2 * word_size + 3;
+    set_site_value(site_index, true, words);
+    EXPECT_EQ(0, words[0]);
+    EXPECT_EQ(0, words[1]);
+    EXPECT_EQ(shift_3, words[2]);
+    EXPECT_EQ(0, words[3]);
+    set_site_value(site_index, false, words);
+    EXPECT_EQ(0, words[0]);
+    EXPECT_EQ(0, words[1]);
+    EXPECT_EQ(0, words[2]);
+    EXPECT_EQ(0, words[3]);
+ 
+    // Test flipping two sites
+    set_site_value(site_index, true, words);
+    set_site_value(2 * word_size + 19, true, words);
+    EXPECT_EQ(0, words[0]);
+    EXPECT_EQ(0, words[1]);
+    EXPECT_EQ(shift_3 | shift_19, words[2]);
+    EXPECT_EQ(0, words[3]);
+    set_site_value(site_index, false, words);
+    EXPECT_EQ(0, words[0]);
+    EXPECT_EQ(0, words[1]);
+    EXPECT_EQ(shift_19, words[2]);
+    EXPECT_EQ(0, words[3]);
+
+    if(word_size > 32) {
+        // Test flipping a site beyond 32 bits
+        site_index = word_size + 41;
+        set_site_value(site_index, true, words);
+        EXPECT_EQ(0, words[0]);
+        EXPECT_EQ(shift_41, words[1]);
+        EXPECT_EQ(shift_19, words[2]);
+        EXPECT_EQ(0, words[3]);
+        set_site_value(site_index, false, words);
+        EXPECT_EQ(0, words[0]);
+        EXPECT_EQ(0, words[1]);
+        EXPECT_EQ(shift_19, words[2]);
+        EXPECT_EQ(0, words[3]);
+    }
+
+
 }
